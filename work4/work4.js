01002cc59ede8fc69f817257938d791b1fd96a60
yhj-12223445899555 <2858500516@qq.com>
2019-10-14 13:14:32 +0800

    work4

--- work4/work4.js
+++ work4/work4.js
@@ -1,27 +1,25 @@
 document.write("<p>金字塔</p>")
-let level = prompt('请设置金字塔的层数')
+let level = prompt('请设置金字塔层数')
 level = parseFloat(level)&&Number(level)
 if (isNaN(level)){
     alert('金字塔的层数是数字')
 }
-for(let i=1;1<=level;++i){
-    let black = level - i;
-    for(let k=0;k<black;++k){
+for(let i=1;i<=level;++i){
+    let blank = level - i;
+    for(let k=0;k<blank;++k){
         document.write('&nbsp');
+    }
+    let star = i * 2 - 1
+    for(let j=0;j<star;++j){
+        document.write('*')
+    }
+    document.write("<br>")
 }
-let star = i * 2 - 1
-for(let j =0;j<star;++j){
-    document.write('*')
-}
-document.write("<br>")
-}
-
 let str = '<table border="1">'
 for(let i=1;i<10;++i){
     str += '<tr>'
     for(var j=1;j<=i;++j){
-        str += '<td>' + j + '*' + i + '=' + ( j * i ) + '</td>'
-
+        str += '<td>' + j + '*' + i + '=' + ( j * i) + '</td>'
 }
 str += '</tr>'
 }

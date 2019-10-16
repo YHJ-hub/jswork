af90cb209ab768a4325bcd00455d1a332e4ba001
yhj-12223445899555 <2858500516@qq.com>
2019-10-16 15:33:37 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,25 @@
+function calculate(){
+    let num = document.getElementById('').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('').value = pi()
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=1;i>0;++i){
+        Str +='<tr>'
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial()
+        let divisor = oddFactorial()
+       
+    }
+    return sum = 2
+
+}
+calculate()

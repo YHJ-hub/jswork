d67f39a5eebd33f5cb2aaaac3a55b1d273be87de
yhj-12223445899555 <2858500516@qq.com>
2019-10-24 12:42:30 +0800

    work8

---
+++ work8/goldbach.js
@@ -0,0 +1,10 @@
+function glodbach(g){
+    let result = document.getElementById('result')
+    let odd = parseInt(document.getElementById('odd').value)
+    if(isNaN(odd)){
+        return false
+    }
+    for(var i=2;i>=2;i+=2)
+    
+
+}
